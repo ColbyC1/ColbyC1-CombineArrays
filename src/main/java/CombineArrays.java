5d25b20a24ca5333bca629ee4537ccb10bb23fe5
Colby Canselo <colbycanselo@gmail.com>
2023-03-03 16:31:23 +0000

    COMBINE ARRAYS

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -5,10 +5,24 @@ public class CombineArrays {
      *
      * @param arr1 an array of ints.
      * @param arr2 an array of ints.
-     * @return an array containing the contents of arr1 followed by the contents of arr2.
+     * @return an array containing the contents of arr1 followed by the contents of
+     *         arr2.
      */
-    public int[] combine(int[] arr1, int[] arr2){
+    public int[] combine(int[] arr1, int[] arr2) {
 
-        return null;
+        int[] arr3 = new int[arr1.length + arr2.length];
+        int index = 0;
+
+        for (int num : arr1) {
+            arr3[index] = num;
+            index++;
+        }
+
+        for (int num : arr2) {
+            arr3[index] = num;
+            index++;
+        }
+
+        return arr3;
     }
 }
